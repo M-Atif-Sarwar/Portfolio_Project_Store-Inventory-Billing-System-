d601e02e5b7a0d8c446055ae9a4601362298dc54
M Atif Sarwar <matifsarwar32@gmail.com>
2026-07-26 18:12:36 +0500

    created log procedure

--- SQL_PLSQL/LogTable.sql
+++ SQL_PLSQL/LogTable.sql
@@ -10,3 +10,34 @@ CREATE TABLE activity_log(
 );
 
 COMMIT;
+
+
+CREATE OR REPLACE PROCEDURE log_activity(
+    p_log_type IN VARCHAR2,
+    p_log_module_name IN VARCHAR2,
+    p_log_reference_id IN NUMBER,
+    p_log_message IN VARCHAR2,
+    p_log_error_code IN NUMBER
+) AS
+
+PRAGMA AUTONOMOUS_TRANSACTION;
+
+BEGIN
+
+    INSERT INTO activity_log (
+        Log_Type,
+        Log_ModuleName,
+        Log_ReferenceID,
+        Log_Message,
+        Log_ErrorCode
+    ) VALUES (
+        p_log_type,
+        p_log_module_name,
+        p_log_reference_id,
+        p_log_message,
+        p_log_error_code
+    );
+    
+    COMMIT;
+    
+END log_activity;

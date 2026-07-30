bcd2b6140ea872ffb94e75c03a091005a76e0c76
M Atif Sarwar <matifsarwar32@gmail.com>
2026-07-30 13:54:56 +0500

    insert sample dataset

---
+++ DataSet/SampleDataSet.sql
@@ -0,0 +1,59 @@
+-- Sample inventory data
+-- Product_ID is auto-generated via IDENTITY, not included
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Coca Cola', 'Cherry', '250ml', 1017.32, 0);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Pepsi', 'Original', '500ml', 1477.83, 64);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Sprite', 'Lemon', '500ml', 1112.43, 0);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Fanta', 'Orange', '1.5L', 194.82, 67);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('7Up', 'Original', '1L', 171.49, 9);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Mountain Dew', 'Original', '500ml', 1076.89, 156);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Lays Chips', 'Masala', 'Small', 302.49, 58);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Kurkure', 'Chutney Chaska', 'Small', 1391.97, 0);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Doritos', 'Nacho Cheese', 'Small', 1266.22, 90);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Pringles', 'Sour Cream', 'Large', 935.88, 3);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Oreo', 'Original', 'Small Pack', 1457.34, 97);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Lu Biscuits', 'Original', 'Family Pack', 1541.95, 0);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Peek Freans', 'Gluco', 'Small Pack', 1905.0, 8);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Nestle Milk', NULL, '250ml', 2406.95, 0);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Olpers Milk', NULL, '1L', 1753.47, 158);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Tapal Tea', 'Danedar', '190g', 2107.92, 131);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Lipton Tea', 'Yellow Label', '190g', 1841.34, 157);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Nescafe Coffee', 'Classic', '100g', 1805.74, 98);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Knorr Noodles', 'Chicken', 'Single Pack', 920.89, 136);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Maggi Noodles', 'Masala', 'Single Pack', 1932.17, 4);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('National Ketchup', 'Original', '800g', 2296.2, 52);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Shan Masala', 'Karahi', '100g', 1396.13, 120);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Dalda Cooking Oil', NULL, '5L', 1067.48, 107);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Sufi Cooking Oil', NULL, '1L', 253.31, 4);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Nurpur Cheese', 'Cheddar', '200g', 2086.18, 5);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Kolson Chips', 'Salted', 'Small', 1076.42, 154);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Candyland Chocolate', 'Dark', 'Small', 1741.71, 168);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Cadbury Dairy Milk', 'Original', 'Large', 2253.86, 184);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Kit Kat', 'Original', 'Small', 1027.5, 0);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Islamabad Salt', NULL, '800g', 516.99, 122);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('National Foods Rice', 'Basmati', '1kg', 883.13, 0);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Guard Rice', 'Basmati', '1kg', 1364.72, 167);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Sooper Rusk', 'Original', 'Family Pack', 2192.11, 48);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('EBM Bread', 'Brown', 'Standard', 1525.58, 39);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Meezan Honey', NULL, '500g', 1226.97, 46);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Shezan Juice', 'Mango', '1L', 1863.86, 187);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Nestle Juice', 'Mango', '250ml', 552.78, 102);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Gourmet Bread', 'White', 'Standard', 1907.45, 2);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Safeguard Soap', 'Original', 'Small', 2275.23, 67);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Lifebuoy Soap', 'Original', 'Small', 1552.41, 59);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Head & Shoulders Shampoo', 'Anti-Dandruff', '400ml', 1862.69, 9);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Sunsilk Shampoo', 'Black Shine', '400ml', 1840.96, 81);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Colgate Toothpaste', 'Original', '150g', 524.43, 98);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Sensodyne Toothpaste', 'Original', '75g', 2389.75, 66);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Surf Excel Detergent', 'Original', '500g', 1201.7, 133);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Ariel Detergent', 'Original', '1kg', 2277.54, 174);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Vim Dishwash Bar', 'Original', 'Small', 2278.95, 61);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Lux Soap', 'Original', 'Small', 1113.12, 32);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Bonaqua Water', NULL, '1.5L', 1033.4, 195);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Nestle Pure Life Water', NULL, '500ml', 2483.13, 0);
+INSERT INTO inventory (Product_Name, Product_Flavor, Product_Size, Product_Price, Product_Quantity) VALUES ('Chai Nashta Rusk', 'Original', 'Family Pack', 1548.35, 131);
+
+COMMIT;
+
+SELECT * FROM inventory;
+
+SELECT * FROM activity_log;

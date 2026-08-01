cf9c3ba6fa4885ec479fc76f93e3ff072f793ac3
M Atif Sarwar <matifsarwar32@gmail.com>
2026-08-01 20:35:40 +0500

    enable schema  for ord and enable object

---
+++ Oracle_REST_DataServices/ORDS_Schema_Enabaled.sql
@@ -0,0 +1,23 @@
+BEGIN 
+    ORDS.ENABLE_SCHEMA(
+        p_enabled => TRUE,      
+        p_schema => 'Atif_Sarwar',
+        p_url_mapping_type => 'BASE_PATH',
+        p_url_mapping_pattern => 'atif_sarwar',
+        p_auto_rest_auth => FALSE
+    );
+
+END;
+/
+
+BEGIN
+  ORDS.ENABLE_OBJECT(
+    p_enabled      => TRUE,
+    p_schema       => 'ATIF_SARWAR',
+    p_object       => 'INVENTORY',
+    p_object_type  => 'TABLE',
+    p_object_alias => 'inventory'
+  );
+  COMMIT;
+END;
+/
